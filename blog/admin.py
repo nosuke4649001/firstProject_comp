7edaca91797d48aeeedd00954b7cfe25c6845326
nosuke4649001 <88269564+nosuke4649001@users.noreply.github.com>
2021-09-10 03:18:15 +0900

    Update admin.py

--- blog/admin.py
+++ blog/admin.py
@@ -1,6 +1,7 @@
 from django.contrib import admin
-
+from markdownx.admin import MarkdownxModelAdmin
 # Register your models here.
 from .models import BlogPost
 
-admin.site.register(BlogPost)
+admin.site.register(BlogPost, MarkdownxModelAdmin)
+
