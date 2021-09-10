84dd670aded06d3e7178819e8511b4d964835423
nosuke4649001 <88269564+nosuke4649001@users.noreply.github.com>
2021-09-10 15:17:05 +0900

    Update app.py

--- blog/templatetags/app.py
+++ blog/templatetags/app.py
@@ -1,24 +1,15 @@
-<<<<<<< HEAD
-=======
 # -*- coding: utf-8 -*-
 
 # -*- coding: utf-8 -*-
 
->>>>>>> b0254daaf1feb3a7a4673f4c3cbb6ebbd8bfd450
 from django import template
 from django.utils.safestring import mark_safe
 import markdown
 from markdownx.utils import markdownify
 from markdownx.settings import (
-<<<<<<< HEAD
-            MARKDOWNX_MARKDOWN_EXTENSIONS,
-                MARKDOWNX_MARKDOWN_EXTENSION_CONFIGS
-                )
-=======
     MARKDOWNX_MARKDOWN_EXTENSIONS,
     MARKDOWNX_MARKDOWN_EXTENSION_CONFIGS
 )
->>>>>>> b0254daaf1feb3a7a4673f4c3cbb6ebbd8bfd450
 from markdown.extensions import Extension
 
 register = template.Library()
@@ -26,15 +17,10 @@ register = template.Library()
 
 @register.filter
 def markdown_to_html(content):
-<<<<<<< HEAD
-    return mark_safe(markdownify(content))
-
-=======
     """マークダウンをhtmlに変換する。"""
     return mark_safe(markdownify(content))
 
 
->>>>>>> b0254daaf1feb3a7a4673f4c3cbb6ebbd8bfd450
 class EscapeHtml(Extension):
     def extendMarkdown(self, md):
         md.preprocessors.deregister('html_block')
@@ -42,12 +28,6 @@ class EscapeHtml(Extension):
 
 @register.filter
 def markdown_to_html_with_escape(text):
-<<<<<<< HEAD
-    extensions = MARKDOWNX_MARKDOWN_EXTENSIONS + [EscapeHtml()]
-    html = markdown.markdown(text, extensions=extensions, extension_configs=MARKDOWNX_MARKDOWN_EXTENSION_CONFIGS)
-    return mark_safe(html)
-
-=======
     """マークダウンをhtmlに変換する。
 
     生のHTMLやCSS、JavaScript等のコードをエスケープした上で、マークダウンをHTMLに変換します。
@@ -57,4 +37,3 @@ def markdown_to_html_with_escape(text):
     extensions = MARKDOWNX_MARKDOWN_EXTENSIONS + [EscapeHtml()]
     html = markdown.markdown(text, extensions=extensions, extension_configs=MARKDOWNX_MARKDOWN_EXTENSION_CONFIGS)
     return mark_safe(html)
->>>>>>> b0254daaf1feb3a7a4673f4c3cbb6ebbd8bfd450
