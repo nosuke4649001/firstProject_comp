7493fff67c6921244d0cf62d13ed44f82ea6e83a
yukitoros <ryu_inazuma11@outlook.jp>
2021-10-11 00:42:29 +0900

    settings_change

--- firstProject/settings.py
+++ firstProject/settings.py
@@ -27,8 +27,8 @@ SECRET_KEY = 'django-insecure-rrierj6*4stjf90&5v!-s00(mo#0%jd!q$w(8#@idtoq$&e-k8
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-#ALLOWED_HOSTS = ["yukitoro.com"]
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ["yukitoro.com"]
+#ALLOWED_HOSTS = []
 
 
 # Application definition
@@ -78,7 +78,7 @@ WSGI_APPLICATION = 'firstProject.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/3.2/ref/settings/#databases
 
-
+"""
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.sqlite3',
@@ -98,7 +98,7 @@ DATABASES = {
             
     }
 }
-"""
+
 # 見出しを使う場合は、tocを入れましょう。
 MARKDOWNX_MARKDOWN_EXTENSIONS = [
     'markdown.extensions.toc',
